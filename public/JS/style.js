4140c28454835770ac7cbfe3eefef0df2836e8f9
rodennis <rev.rltodd@gmail.com>
2021-10-20 16:20:39 -0500

    finished the email form front and back end, still need to work on the responsivness

--- public/JS/style.js
+++ public/JS/style.js
@@ -3,9 +3,9 @@ const lastName = document.getElementById('lastName');
 const phone = document.getElementById('phone');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
-const submit = document.getElementById('submit');
+const submit = document.querySelector('.contactForm');
 
-submit.addEventListener('click', (e) => {
+submit.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = {
         firstName: firstName.value,
@@ -14,21 +14,23 @@ submit.addEventListener('click', (e) => {
         email: email.value,
         message: message.value
     }
+    console.log(formData)
+
     let xhr = new XMLHttpRequest();
     xhr.open('POST', '/');
     xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function () {
-        console.log(xhr.responseText)
+    xhr.onload = function() {
+        console.log(xhr.responseText);
         if(xhr.responseText == 'success') {
-            document.getElementById('success').show;
+            alert('Email sent');
             firstName.value = '';
             lastName.value = '';
             phone.value = '';
             email.value = '';
             message.value = '';
-        }
-        else{
-            alert('Something went wrong.')
+        }else{
+            alert('Something went wrong!')
         }
     }
+    xhr.send(JSON.stringify(formData))
 })  
--- server.js
+++ server.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const app = express()
-
+const nodemailer = require('nodemailer')
 const PORT = 8080
 
 app.use(express.static('views'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'Dev.rodneytodd@gmail.com',
+            pass: 'Todd@123456789'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'rev.rltodd@gmail.com',
+        subject: `Message from ${req.body.email}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            console.log(error)
+            res.send('error')
+        }
+        else {
+            console.log('Email Sent!')
+            res.send('success')
+        }
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
