44f9e3fbe7030c44148630c704650b906b4630c1
rodennis <rev.rltodd@gmail.com>
2021-10-22 17:29:16 -0500

    fixed responsiveness, changed font, changed tech icons

--- public/JS/style.js
+++ public/JS/style.js
@@ -38,3 +38,23 @@ submit.addEventListener('submit', (e) => {
 exit.addEventListener('click', () => {
     document.getElementById('success').className='hide'
 })
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
